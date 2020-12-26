2b1b0a43d3b19e3f469fb5e0842a90dd6be70faf
amadejpapez <63184600+amadejpapez@users.noreply.github.com>
2020-12-26 15:32:55 +0100

    version 2.0

--- EndOfYearCountdown_bot_v1.0.py
+++ EndOfYearCountdown_bot_v1.0.py
@@ -1,40 +1,42 @@
 # IMPORT MODULES
-# if you see an error you need to first install that module
+# if you see an error you need to first install them
 import tweepy
 import emoji
 import schedule
-import os
 import time
 from datetime import date
 
-time.sleep(26400)
 
-#Sunday
-api.update_status(emoji.emojize(":clock12: 326 days until 2021", use_aliases=True))
-time.sleep(3600)
-api.update_status(emoji.emojize(":hourglass_flowing_sand: 45 weeks until 2021", use_aliases=True))
-time.sleep(82800)
+# CALCULATE DAYS UNTIL 2021
+def calculate_days():
+    date1 = date.today()
+    date2 = date(2021, 1, 1)
+    days_until = date2 - date1
+    api.update_status(emoji.emojize(":clock12: {} days till the end of year".format(days_until.days), use_aliases=True))
 
-#Monday
-api.update_status(emoji.emojize(":clock12: 327 days until 2021", use_aliases=True))
-time.sleep(86400)
+schedule.every().monday.at("06:00").do(calculate_days)
+schedule.every().tuesday.at("06:00").do(calculate_days)
+schedule.every().wednesday.at("06:00").do(calculate_days)
+schedule.every().thursday.at("06:00").do(calculate_days)
+schedule.every().friday.at("06:00").do(calculate_days)
+schedule.every().saturday.at("06:00").do(calculate_days)
+schedule.every().sunday.at("06:00").do(calculate_days)
 
-#Tuesday
-api.update_status(emoji.emojize(":clock12: 328 days until 2021", use_aliases=True))
-time.sleep(86400)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
-#Wednesday
-api.update_status(emoji.emojize(":clock12: 329 days until 2021", use_aliases=True))
-time.sleep(86400)
 
-#Thursday
-api.update_status(emoji.emojize(":clock12: 330 days until 2021", use_aliases=True))
-time.sleep(86400)
+# CALCULATE WEEKS UNTIL 2021
+def calculate_weeks():
+    date1 = date.today()
+    date2 = date(2021, 1, 1)
+    days_until = date2 - date1
+    weeks_until = days_until.days // 7 
+    api.update_status(emoji.emojize(":date: {} weeks till the end of year".format(weeks_until), use_aliases=True))
 
-#Friday
-api.update_status(emoji.emojize(":clock12: 331 days until 2021", use_aliases=True))
-time.sleep(86400)
+schedule.every().sunday.at("06:15").do(calculate_weeks)
 
-#Saturday
-api.update_status(emoji.emojize(":clock12: 332 days until 2021", use_aliases=True))
-time.sleep(86400)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
