aaf3c33ba4ad8a0682393770302a920a28d8170e
amadejpapez <63184600+amadejpapez@users.noreply.github.com>
2020-12-26 15:53:09 +0100

    version 3.0

--- EndOfYearCountdown bot.py
+++ EndOfYearCountdown bot.py
@@ -1,41 +1,38 @@
 # IMPORT MODULES
-# if you see an error you need to first install them
 import tweepy
 import emoji
 import schedule
 import time
 from datetime import date
 
+# END OF YEAR COUNTDOWN BOT
+def endOfYearCountdown():
+    # calculate how many days till the end of the year
+    todaysDate = date.today()
+    endOfYearDate = date(2021, 1, 1)
+    subtractDates = endOfYearDate - todaysDate
+    daysUntil = subtractDates.days
+    # check if the bot should tweet how many weeks till the end of the year and tweet that if appropriate
+    if(daysUntil % 7 == 0):
+        # calculate how many weeks till the end of the year
+        weeksUntil = daysUntil / 7
+        if(weeksUntil > 1):
+            api.update_status(emoji.emojize(":date: {} weeks till the end of the year".format(int(weeksUntil)), use_aliases=True))
+        if(weeksUntil == 1):
+            # if it's the last week of the year
+            api.update_status(emoji.emojize(":date: 1 week till the end of the year", use_aliases=True))
+    if(daysUntil % 7 != 0):
+        # if tweeting weeks is not appropriate tweet how many days till the end of the year
+        if(daysUntil > 1):
+            api.update_status(emoji.emojize(":clock12: {} days till the end of the year".format(daysUntil), use_aliases=True))
+        if(daysUntil == 1):
+            # if it's the last day of the year
+            api.update_status(emoji.emojize("Enjoy your last day of the year! :grinning:", use_aliases=True))
+    if(daysUntil == 0):
+        # if it's the first day of the year
+        api.update_status(emoji.emojize("It’s the first day of 2021! Wishing you health, wealth, and happiness in the new year ahead. Happy New Year! :tada:", use_aliases=True))
 
-# CALCULATE DAYS UNTIL 2021
-def calculate_days():
-    date1 = date.today()
-    date2 = date(2021, 1, 1)
-    days_until = date2 - date1
-    api.update_status(emoji.emojize(":clock12: {} days till the end of year".format(days_until.days), use_aliases=True))
-
-schedule.every().monday.at("06:00").do(calculate_days)
-schedule.every().tuesday.at("06:00").do(calculate_days)
-schedule.every().wednesday.at("06:00").do(calculate_days)
-schedule.every().thursday.at("06:00").do(calculate_days)
-schedule.every().friday.at("06:00").do(calculate_days)
-schedule.every().saturday.at("06:00").do(calculate_days)
-schedule.every().sunday.at("06:00").do(calculate_days)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
-# CALCULATE WEEKS UNTIL 2021
-def calculate_weeks():
-    date1 = date.today()
-    date2 = date(2021, 1, 1)
-    days_until = date2 - date1
-    weeks_until = days_until.days // 7 
-    api.update_status(emoji.emojize(":date: {} weeks till the end of year".format(weeks_until), use_aliases=True))
-
-schedule.every().sunday.at("06:15").do(calculate_weeks)
+schedule.every().day.at("06:00").do(endOfYearCountdown)
 
 while True:
     schedule.run_pending()
