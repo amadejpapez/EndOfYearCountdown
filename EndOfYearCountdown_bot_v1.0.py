38ff39fc2241a5b16e263132aad382340713c67b
amadejpapez <63184600+amadejpapez@users.noreply.github.com>
2020-12-26 15:20:15 +0100

    First release

---
+++ EndOfYearCountdown_bot_v1.0.py
@@ -0,0 +1,40 @@
+# IMPORT MODULES
+# if you see an error you need to first install that module
+import tweepy
+import emoji
+import schedule
+import os
+import time
+from datetime import date
+
+time.sleep(26400)
+
+#Sunday
+api.update_status(emoji.emojize(":clock12: 326 days until 2021", use_aliases=True))
+time.sleep(3600)
+api.update_status(emoji.emojize(":hourglass_flowing_sand: 45 weeks until 2021", use_aliases=True))
+time.sleep(82800)
+
+#Monday
+api.update_status(emoji.emojize(":clock12: 327 days until 2021", use_aliases=True))
+time.sleep(86400)
+
+#Tuesday
+api.update_status(emoji.emojize(":clock12: 328 days until 2021", use_aliases=True))
+time.sleep(86400)
+
+#Wednesday
+api.update_status(emoji.emojize(":clock12: 329 days until 2021", use_aliases=True))
+time.sleep(86400)
+
+#Thursday
+api.update_status(emoji.emojize(":clock12: 330 days until 2021", use_aliases=True))
+time.sleep(86400)
+
+#Friday
+api.update_status(emoji.emojize(":clock12: 331 days until 2021", use_aliases=True))
+time.sleep(86400)
+
+#Saturday
+api.update_status(emoji.emojize(":clock12: 332 days until 2021", use_aliases=True))
+time.sleep(86400)
