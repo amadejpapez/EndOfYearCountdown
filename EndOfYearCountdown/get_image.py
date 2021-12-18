df06ff615354fef36b94627d640ebb3b3156ce72
amadejpapez <amadej.papez@icloud.com>
2021-12-18 19:22:31 +0100

    feat: use requests for image download instead of wget

--- EndOfYearCountdown/get_image.py
+++ EndOfYearCountdown/get_image.py
@@ -1,15 +1,15 @@
 import os
 
-import wget
+import requests
 from auth_secrets import keys
 from PIL import Image
 from pyunsplash import PyUnsplash
 
-dirPath = os.path.dirname(os.path.realpath(__file__))
+DIRPATH = os.path.dirname(os.path.realpath(__file__))
 
 
 def get_image(season):
-    # get image from Unsplash
+    # request image from Unsplash
     unsplash = PyUnsplash(api_key=keys["PyUnsplash"]["api_key"])
     image = unsplash.photos(
         type_="random",
@@ -19,24 +19,23 @@ def get_image(season):
         query=season,
     )
 
-    if os.path.exists(f"{dirPath}/photo.jpg"):
-        # remove previous "photo"
-        os.remove(f"{dirPath}/photo.jpg")
-
     optimize_image(image, season)
 
 
 def optimize_image(image, season):
     for image in image.entries:
         # download image to the current folder
-        wget.download(image.link_download, out=f"{dirPath}/photo.jpg")
+        img_data = requests.get(image.link_download).content
 
-        if os.path.getsize(f"{dirPath}/photo.jpg") >= 3200000:
+        with open(f"{DIRPATH}/photo.jpg", "wb") as myFile:
+            myFile.write(img_data)
+
+        if os.path.getsize(f"{DIRPATH}/photo.jpg") >= 3200000:
             # if image is bigger than 3.2 MB, take another one
             get_image(season)
 
-        while os.path.getsize(f"{dirPath}/photo.jpg") >= 3072000:
+        while os.path.getsize(f"{DIRPATH}/photo.jpg") >= 3072000:
             # if image is larger than 3.072 MB, optimize it as larger size will cause an error
-            image = Image.open(f"{dirPath}/photo.jpg")
+            image = Image.open(f"{DIRPATH}/photo.jpg")
             image.resize((1080, 566), Image.ANTIALIAS)
-            image.save(f"{dirPath}/photo.jpg", quality=80)
+            image.save(f"{DIRPATH}/photo.jpg", quality=80)
