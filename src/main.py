6d39724092fef0b8cc8c8dc7315352011ff40bb3
amadejpapez <amadej.papez@icloud.com>
2022-01-08 00:02:16 +0100

    style: improved readability and formatting

--- src/main.py
+++ src/main.py
@@ -2,43 +2,28 @@ from datetime import date, timedelta
 
 from twitter import tweet, update_profile_picture
 
+
 def get_season(month):
     if month in range(3, 5):
-        season = "spring"
-    elif month in range(6, 8):
-        season = "summer"
-    elif month in range(9, 11):
-        season = "fall"
-    else:
-        season = "winter"
+        return "spring"
+    if month in range(6, 8):
+        return "summer"
+    if month in range(9, 11):
+        return "fall"
 
-    return season
+    return "winter"
+
 
 def ordinal_number(num):
-    if int(num) < 20:
-        if num == "1":
-            num += "st"
-        elif num == "2":
-            num += "nd"
-        elif num == "3":
-            num += "rd"
-        else:
-            num += "th"
-    else:
-        if num[-2] == "1":
-            num += "th"
-        else:
-            unit = num[-1]
-            if unit == "1":
-                num += "st"
-            elif unit == "2":
-                num += "nd"
-            elif unit == "3":
-                num += "rd"
-            else:
-                num += "th"
+    if int(num) < 20 and num[-2] != "1":
+        if num == "1" or num[-1] == "1":
+            return "st"
+        if num == "2" or num[-1] == "2":
+            return "nd"
+        if num == "3" or num[-1] == "3":
+            return "rd"
 
-    return num
+    return "th"
 
 
 current_day = date.today()
@@ -55,17 +40,26 @@ season = get_season(date.today().month)
 if previous_day.isocalendar().week < current_day.isocalendar().week:
     weeks_all = int((end_of_year_day - date(current_year, 1, 1)).days / 7)
     week_num = current_day.isocalendar().week
-    week_num_format = ordinal_number(str(week_num))
+    week_num_format = str(week_num) + ordinal_number(str(week_num))
     weeks_left = weeks_all - week_num
 
-    tweet(f"{week_num_format} week of {current_year} is starting with {weeks_left} weeks ({days_left} days) left this year.", f"{season} fun animals nature")
+    tweet(
+        f"{week_num_format} week of {current_year} is starting with {weeks_left} weeks ({days_left} days) left this year. :sun:",
+        f"{season} fun animals nature",
+    )
 
 elif current_day == date(current_year, 12, 25):
-    tweet(f"Merry Christmas! :christmas_tree: Next week we will be in {next_year}!", "christmas")
+    tweet(
+        f"Merry Christmas! :christmas_tree: Next week we will be in {next_year}!",
+        "christmas",
+    )
 
 elif current_day == date(current_year, 12, 31):
     tweet(f"The last day of {current_year} has finally come! :partying_face:")
 
 elif current_day == date(current_year, 1, 1):
-    tweet(f"{current_year} is here! I wish you all health, love, and happiness in the new year. Happy New Year! :tada:", "new year")
+    tweet(
+        f"{current_year} is here! I wish you all health, love, and happiness in the new year. Happy New Year! :tada:",
+        "new year",
+    )
     update_profile_picture(current_year)
--- src/twitter.py
+++ src/twitter.py
@@ -26,8 +26,8 @@ File structure:
 """
 
 location = os.path.abspath(os.path.join(__file__, "../../../auth_secrets.json"))
-with open(location, "r", encoding="utf-8") as myFile:
-    keys = json.load(myFile)
+with open(location, "r", encoding="utf-8") as authFile:
+    keys = json.load(authFile)
 
 # Migrate to Twitter API v2 when it starts supporting tweets with images
 # https://trello.com/c/Zr9zDrJx/109-replacement-of-media-uploads-functionality
@@ -46,22 +46,26 @@ DIRPATH = os.path.dirname(os.path.realpath(__file__))
 
 def request_images(query):
     # get 5 images from Unsplash that fit into the given query
-    images = requests.get("https://api.unsplash.com/photos/random", params={
-        "client_id": keys["PyUnsplash"]["api_key"],
-        "count": 5,
-        "orientation": "landscape",
-        "query": query
-    }).json()
+    images = requests.get(
+        "https://api.unsplash.com/photos/random",
+        params={
+            "client_id": keys["PyUnsplash"]["api_key"],
+            "count": 5,
+            "orientation": "landscape",
+            "query": query,
+        },
+    ).json()
 
     get_image(images, query)
+
 
 def get_image(images, query):
     # takes image that is under 5MB, if all are over that request 5 new ones
     for img in images:
         img_data = requests.get(img["urls"]["regular"]).content
 
-        with open(f"{DIRPATH}/photo.jpg", "wb", encoding="utf-8") as myFile:
-            myFile.write(img_data)
+        with open(f"{DIRPATH}/photo.jpg", "wb") as imgFile:
+            imgFile.write(img_data)
 
         if os.path.getsize(f"{DIRPATH}/photo.jpg") < 5242880:
             return img["urls"]["regular"]
@@ -70,7 +74,7 @@ def get_image(images, query):
 
 
 def tweet(text, query = None):
-    if query == None:
+    if query is None:
         # tweet without an image
         API.update_status(emoji.emojize(text, use_aliases=True))
     else:
@@ -80,8 +84,9 @@ def tweet(text, query = None):
 
         API.update_status(
             media_ids=[img_upload.media_id],
-            status=emoji.emojize(text, use_aliases=True)
+            status=emoji.emojize(text, use_aliases=True),
         )
+
 
 def update_profile_picture(current_year):
     API.update_profile_image(f"{DIRPATH}/{current_year}.jpg")
