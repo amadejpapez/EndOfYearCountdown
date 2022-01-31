0be05c010ab979720173a44a390e24c78b7c8ed8
amadejpapez <amadej.papez@icloud.com>
2022-01-31 16:33:27 +0100

    feat: completely remove images, migrate to API v2
    - completely remove functionality to get images from PyUnsplash because they do not add much to the tweets
    - with tweeting with images being removed, migrate to Twitter API v2
    - also remove functionality to update the profile picture because it seems that is not supported yet in v2

--- src/main.py
+++ src/main.py
@@ -3,17 +3,6 @@ from datetime import date, timedelta
 from twitter import tweet, update_profile_picture
 
 
-def get_season(month):
-    if month in range(3, 5):
-        return "spring"
-    if month in range(6, 8):
-        return "summer"
-    if month in range(9, 11):
-        return "fall"
-
-    return "winter"
-
-
 def ordinal_number(num):
     if int(num) not in range(4, 21):
         if num[-1] == "1":
@@ -35,8 +24,6 @@ end_of_year_day = date(current_year, 12, 31)
 
 days_left = (end_of_year_day - current_day).days
 
-season = get_season(date.today().month)
-
 if previous_day.isocalendar().week < current_day.isocalendar().week:
     weeks_all = int((end_of_year_day - date(current_year, 1, 1)).days / 7)
     week_num = current_day.isocalendar().week
@@ -44,14 +31,12 @@ if previous_day.isocalendar().week < current_day.isocalendar().week:
     weeks_left = weeks_all - week_num
 
     tweet(
-        f"{week_num_format} week of {current_year} is starting with {weeks_left} weeks ({days_left} days) left this year. :sun:",
-        # f"{season} fun animals nature",
+        f"{week_num_format} week of {current_year} is starting with {weeks_left} weeks ({days_left} days) left this year. :sun:"
     )
 
 elif current_day == date(current_year, 12, 25):
     tweet(
-        f"Merry Christmas! :christmas_tree: Next week we will be in {next_year}!",
-        "christmas",
+        f"Merry Christmas! :christmas_tree: Next week we will be in {next_year}!"
     )
 
 elif current_day == date(current_year, 12, 31):
@@ -59,7 +44,6 @@ elif current_day == date(current_year, 12, 31):
 
 elif current_day == date(current_year, 1, 1):
     tweet(
-        f"{current_year} is here! I wish you all health, love, and happiness in the new year. Happy New Year! :tada:",
-        "new year",
+        f"{current_year} is here! I wish you all health, love, and happiness in the new year. Happy New Year! :tada:"
     )
     update_profile_picture(current_year)
--- src/twitter.py
+++ src/twitter.py
@@ -12,9 +12,6 @@ File structure:
         "api_key_secret"        : "x",
         "access_token"          : "x",
         "access_token_secret"   : "x"
-    },
-    "PyUnsplash" : {
-        "api_key"               : "x"
     }
 }
 -----------------------------
@@ -24,71 +21,23 @@ import json
 import os
 
 import emoji
-import requests
 import tweepy
 
-location = os.path.abspath(os.path.join(__file__, "../../../auth_secrets.json"))
-with open(location, "r", encoding="utf-8") as authFile:
-    keys = json.load(authFile)
+AUTH_LOC = os.path.abspath(os.path.join(__file__, "../../../auth_secrets.json"))
+with open(AUTH_LOC, "r", encoding="utf-8") as authFile:
+    KEYS = json.load(authFile)
 
-# Migrate to Twitter API v2 when it starts supporting tweets with images
-# https://trello.com/c/Zr9zDrJx/109-replacement-of-media-uploads-functionality
-auth = tweepy.OAuthHandler(
-    consumer_key=keys["EndOfYearCountdown"]["api_key"],
-    consumer_secret=keys["EndOfYearCountdown"]["api_key_secret"]
+API = tweepy.Client(
+    consumer_key=KEYS["EndOfYearCountdown"]["api_key"],
+    consumer_secret=KEYS["EndOfYearCountdown"]["api_key_secret"],
+    access_token=KEYS["EndOfYearCountdown"]["access_token"],
+    access_token_secret=KEYS["EndOfYearCountdown"]["access_token_secret"]
 )
-auth.set_access_token(
-    key=keys["EndOfYearCountdown"]["access_token"],
-    secret=keys["EndOfYearCountdown"]["access_token_secret"]
-)
-API = tweepy.API(auth)
 
 DIRPATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def request_images(query):
-    # get 3 images from Unsplash that fit into the given query
-    response = requests.get(
-        "https://api.unsplash.com/photos/random",
-        params={
-            "client_id": keys["PyUnsplash"]["api_key"],
-            "count": 3,
-            "orientation": "landscape",
-            "query": query,
-        },
-    ).json()
-
-    get_image(response, query)
-
-
-def get_image(response, query):
-    # takes image that is under 5MB, if all are over that request 3 new ones
-    for img in response:
-        img_data = requests.get(img["urls"]["regular"]).content
-
-        with open(f"{DIRPATH}/photo.jpg", "wb") as img_file:
-            img_file.write(img_data)
-
-        if os.path.getsize(f"{DIRPATH}/photo.jpg") < 5242880:
-            return img["urls"]["regular"]
-
-    request_images(query)
-
-
-def tweet(text, query=None):
-    if query is None:
-        # tweet without an image
-        API.update_status(emoji.emojize(text, use_aliases=True))
-    else:
-        # tweet with an image
-        request_images(query)
-        img_upload = API.media_upload(f"{DIRPATH}/photo.jpg")
-
-        API.update_status(
-            media_ids=[img_upload.media_id],
-            status=emoji.emojize(text, use_aliases=True),
-        )
-
-
-def update_profile_picture(current_year):
-    API.update_profile_image(f"{DIRPATH}/{current_year}.jpg")
+def tweet(text):
+    API.create_tweet(
+        text=emoji.emojize(text, use_aliases=True),
+    )
